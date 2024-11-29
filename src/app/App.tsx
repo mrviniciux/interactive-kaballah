6c129d009b1bf7cb880a6e9b4fac86035aa15298
mrviniciux <httpmarks@gmail.com>
2024-11-29 15:13:30 -0300

    feat(under-construction): alert

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,56 +1,60 @@
-import BiggerSephirot from '../components/sephirot/bigger/BiggerSephirot';
-import Sephirot from '../components/sephirot/Sephirot';
+import BiggerSephirot from '../components/Sephirot/bigger/BiggerSephirot';
+import Sephirot from '../components/Sephirot/Sephirot';
+import PageUnderConstruction from '../components/UnderConstruction/UnderConstruction';
 
 function App() {
   return (
-    <section className="App">
-      <BiggerSephirot
-        world={{
-          title: '  Briah (Mundo Criativo)',
-          aspect: '     Onisciência',
-        }}
-        regent={{
-          title: '   Tronos - Tzafquiel',
-          name: 'YHVH ELOHIM',
-          defect: 'Avareza',
-          fontColor: 'white',
-        }}
-        sephirot={{
-          name: '  Binah',
-          valor: 'Entendimento',
-          backgroundColor: 'rgb(217, 217, 217)',
-          strokeColor: 'white',
-        }}
-        planet={{
-          number: 3,
-          backgroundColor: 'rgb(73, 73, 73)',
-        }}
-      />
+    <>
+      <PageUnderConstruction />
+      <section className="App">
+        <BiggerSephirot
+          world={{
+            title: '  Briah (Mundo Criativo)',
+            aspect: '     Onisciência',
+          }}
+          regent={{
+            title: '   Tronos - Tzafquiel',
+            name: 'YHVH ELOHIM',
+            defect: 'Avareza',
+            fontColor: 'white',
+          }}
+          sephirot={{
+            name: '  Binah',
+            valor: 'Entendimento',
+            backgroundColor: 'rgb(217, 217, 217)',
+            strokeColor: 'white',
+          }}
+          planet={{
+            number: 3,
+            backgroundColor: 'rgb(73, 73, 73)',
+          }}
+        />
 
-      <Sephirot
-        regent={{
-          title: '   Anjos - Gabriel',
-          name: 'Shaddai El Chai',
-          defect: 'Preguiça',
-          fontColor: 'white',
-          strokeColor: 'white',
-          backgroundColor: 'rgb(138, 41, 135)',
-        }}
-        sephirot={{
-          name: 'Yesod',
-          valor: 'Fundação',
-          backgroundColor: 'rgb(205, 172, 209)',
-          strokeColor: 'white',
-        }}
-        planet={{
-          icon: '☽',
-          number: 9,
-          strokeColor: 'white',
-          fontColor: 'white',
-          backgroundColor: 'rgb(138, 41, 135)',
-        }}
-      />
-    </section>
+        <Sephirot
+          regent={{
+            title: '   Anjos - Gabriel',
+            name: 'Shaddai El Chai',
+            defect: 'Preguiça',
+            fontColor: 'white',
+            strokeColor: 'white',
+            backgroundColor: 'rgb(138, 41, 135)',
+          }}
+          sephirot={{
+            name: 'Yesod',
+            valor: 'Fundação',
+            backgroundColor: 'rgb(205, 172, 209)',
+            strokeColor: 'white',
+          }}
+          planet={{
+            icon: '☽',
+            number: 9,
+            strokeColor: 'white',
+            fontColor: 'white',
+            backgroundColor: 'rgb(138, 41, 135)',
+          }}
+        />
+      </section>
+    </>
   );
 }
 
---
+++ src/components/UnderConstruction/UnderConstruction.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const PageUnderConstruction: React.FC = () => {
+  return (
+    <div style={styles.container}>
+      <p style={styles.text}>🚧 Page under construction 🚧</p>
+    </div>
+  );
+};
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    width: '100%',
+    backgroundColor: '#F5DEB3', // Bege
+    padding: '10px 0',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    textAlign: 'center',
+    zIndex: 1000, // Para garantir que esteja acima de outros elementos
+  },
+  text: {
+    margin: 0,
+    fontSize: '18px',
+    fontWeight: 'bold',
+    color: '#000', // Preto para contraste
+  },
+};
+
+export default PageUnderConstruction;
